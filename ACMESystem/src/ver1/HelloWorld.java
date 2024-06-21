9a33986190c99b8b3c7a4b7d5f989d16781748f5
Hannah Hall <hannhall@valdosta.edu>
2024-06-21 16:20:01 -0400

    Add information to HelloWorld

--- ACMESystem/src/ver1/HelloWorld.java
+++ ACMESystem/src/ver1/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from Eclipse");
+		System.out.println("Hannah is working locally on her branch");
 
 	}
 
