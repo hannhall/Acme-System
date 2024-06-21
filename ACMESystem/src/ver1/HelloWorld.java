b39767606303b7eec3fa26abebf656b0a878bc02
Hannah Hall <hannhall@valdosta.edu>
2024-06-21 17:58:57 -0400

    Added whatFor

--- ACMESystem/src/ver1/HelloWorld.java
+++ ACMESystem/src/ver1/HelloWorld.java
@@ -13,5 +13,10 @@ public class HelloWorld {
 		String Msg = "Hey Now!";
 		System.out.println(Msg);
 	}
+	
+	public static void whatFor() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
 
 }
